968ff14364cb78fa9943d50387690b65f17e9d43
unknown <erlangga.prahama@proton.me>
2026-09-25 11:24:17 +0700

    feat: tambah route product catalog dan detail

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Admin\Categories\CategoryManager;
 use App\Livewire\Admin\Products\ProductManager;
+use App\Livewire\ProductCatalog;
+use App\Livewire\ProductDetail;
 
 Route::view('/', 'welcome');
 
@@ -21,4 +23,6 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(fun
     Route::get('/products', ProductManager::class)->name('products.index');
 });
 
+Route::get('/products', ProductCatalog::class)->name('products.index');
+Route::get('/products/{product}', ProductDetail::class)->name('products.show');
 require __DIR__.'/auth.php';

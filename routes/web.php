192f4e8769dcc1fc05ab239fd74f5e2de8a64af1
unknown <erlangga.prahama@proton.me>
2026-09-25 10:07:10 +0700

    feat: tambah route product manager

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Admin\Categories\CategoryManager;
+use App\Livewire\Admin\Products\ProductManager;
 
 Route::view('/', 'welcome');
 
@@ -17,6 +18,7 @@ Route::get('/admin-test', fn () => 'kamu admin!')->middleware(['auth', 'admin'])
 
 Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/categories', CategoryManager::class)->name('categories.index');
+    Route::get('/products', ProductManager::class)->name('products.index');
 });
 
 require __DIR__.'/auth.php';
